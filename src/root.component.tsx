0240c75c32a3f2f14b9148e7733e4fad45223944
Nguyen Van Huan <huantb1812@gmail.com>
2023-06-14 18:46:46 +0800

    up 1

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,12 @@
+
+import './style.css';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <section>{props.name} is mounted!
+  <br />
+  <a href="/angular-app1">angular app1</a>
+  <a href="/angular-app2">angular app2</a>
+  <a href="/welcome">welcome</a>
+
+  </section>;
 }
